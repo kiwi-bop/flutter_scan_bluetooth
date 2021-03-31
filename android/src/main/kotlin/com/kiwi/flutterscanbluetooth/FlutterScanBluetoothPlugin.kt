a76e55764feb6ebcac3d71e129a38904b2758625
freezy <freezy@kodi.tv>
2021-03-31 16:08:43 +0200

    chore: Upgrade to embedding v2.

--- android/src/main/kotlin/com/kiwi/flutterscanbluetooth/FlutterScanBluetoothPlugin.kt
+++ android/src/main/kotlin/com/kiwi/flutterscanbluetooth/FlutterScanBluetoothPlugin.kt
@@ -12,21 +12,22 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.content.pm.PackageManager
 import android.content.pm.PackageManager.PERMISSION_GRANTED
-import android.location.LocationManager
 import android.os.Build
-import androidx.core.app.ActivityCompat
 import android.util.Log
+import androidx.core.app.ActivityCompat
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlutterScanBluetoothPlugin(private val activity: Activity,
-                                 private val channel: MethodChannel) : MethodCallHandler, PluginRegistry.ActivityResultListener, PluginRegistry.RequestPermissionsResultListener {
+class FlutterScanBluetoothPlugin()
+    : FlutterPlugin, ActivityAware, MethodCallHandler, PluginRegistry.ActivityResultListener,
+        PluginRegistry.RequestPermissionsResultListener {
 
     companion object {
         private val TAG = FlutterScanBluetoothPlugin::class.java.name!!
@@ -36,26 +37,10 @@ class FlutterScanBluetoothPlugin(private val activity: Activity,
         private const val ACTION_START_SCAN = "action_start_scan"
         private const val ACTION_STOP_SCAN = "action_stop_scan"
         private const val ACTION_SCAN_STOPPED = "action_scan_stopped"
-
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val channel = MethodChannel(registrar.messenger(), "flutter_scan_bluetooth")
-            val plugin = FlutterScanBluetoothPlugin(registrar.activity(), channel)
-            registrar.addActivityResultListener(plugin)
-            registrar.addRequestPermissionsResultListener(plugin)
-            registrar.addViewDestroyListener {
-                plugin.onViewDestroy()
-                false
-            }
-            channel.setMethodCallHandler(plugin)
-        }
     }
 
-    private var adapter: BluetoothAdapter? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-        (activity.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter
-    } else {
-        BluetoothAdapter.getDefaultAdapter()
-    }
+    private lateinit var activityBinding: ActivityPluginBinding
+    private lateinit var channel: MethodChannel
 
     private var pendingScanResult: Result? = null
     private val receiver = object : BroadcastReceiver() {
@@ -68,6 +53,39 @@ class FlutterScanBluetoothPlugin(private val activity: Activity,
                 channel.invokeMethod(ACTION_SCAN_STOPPED, null)
             }
         }
+    }
+
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        val channel = MethodChannel(binding.binaryMessenger, "flutter_scan_bluetooth")
+        channel.setMethodCallHandler(this)
+    }
+
+    override fun onDetachedFromActivity() {
+        activityBinding.removeRequestPermissionsResultListener(this)
+        onViewDestroy()
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activityBinding = binding
+        binding.addRequestPermissionsResultListener(this)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activityBinding = binding
+        binding.addRequestPermissionsResultListener(this)
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        activityBinding.removeRequestPermissionsResultListener(this)
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    }
+
+    private var adapter: BluetoothAdapter? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+        (activityBinding.activity.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager).adapter
+    } else {
+        BluetoothAdapter.getDefaultAdapter()
     }
 
     fun onViewDestroy() {
@@ -114,7 +132,7 @@ class FlutterScanBluetoothPlugin(private val activity: Activity,
                 true
             }
             GpsUtils.GPS_REQUEST-> {
-                if (GpsUtils(activity).isGpsEnabled) {
+                if (GpsUtils(activityBinding.activity).isGpsEnabled) {
                     scan(pendingScanResult!!)
                 } else {
                     pendingScanResult!!.error("error_no_gps", "Gps need to be turned on to scan BT devices", null)
@@ -146,7 +164,7 @@ class FlutterScanBluetoothPlugin(private val activity: Activity,
         adapter?.cancelDiscovery()
         channel.invokeMethod(ACTION_SCAN_STOPPED, null)
         try {
-            activity.unregisterReceiver(receiver)
+            activityBinding.activity.unregisterReceiver(receiver)
         } catch (e: IllegalArgumentException) {
             throw RuntimeException("Cannot stop Bluetooth scan before starting.")
         }
@@ -155,12 +173,12 @@ class FlutterScanBluetoothPlugin(private val activity: Activity,
 
     private fun scan(result: Result, returnBondedDevices: Boolean = false) {
         if (adapter!!.isEnabled) {
-            if (activity.checkCallingOrSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
-                    == PERMISSION_GRANTED && activity.checkCallingOrSelfPermission(BLUETOOTH_ADMIN)
-                    == PERMISSION_GRANTED&& activity.checkCallingOrSelfPermission(BLUETOOTH)
+            if (activityBinding.activity.checkCallingOrSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                    == PERMISSION_GRANTED && activityBinding.activity.checkCallingOrSelfPermission(BLUETOOTH_ADMIN)
+                    == PERMISSION_GRANTED&& activityBinding.activity.checkCallingOrSelfPermission(BLUETOOTH)
                     == PERMISSION_GRANTED) {
 
-                GpsUtils(activity).turnGPSOn {
+                GpsUtils(activityBinding.activity).turnGPSOn {
                     if (it) {
                         if (adapter!!.isDiscovering) {
                             // Bluetooth is already in modo discovery mode, we cancel to restart it again
@@ -168,7 +186,7 @@ class FlutterScanBluetoothPlugin(private val activity: Activity,
                         }
                         val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
                         filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
-                        activity.registerReceiver(receiver, filter)
+                        activityBinding.activity.registerReceiver(receiver, filter)
 
                         adapter!!.startDiscovery()
                         var bondedDevices: List<Map<String, String>> = arrayListOf()
@@ -187,12 +205,12 @@ class FlutterScanBluetoothPlugin(private val activity: Activity,
                 pendingScanResult = result
             } else {
                 pendingScanResult = result
-                ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, BLUETOOTH, BLUETOOTH_ADMIN), REQUEST_PERMISSION)
+                ActivityCompat.requestPermissions(activityBinding.activity, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, BLUETOOTH, BLUETOOTH_ADMIN), REQUEST_PERMISSION)
             }
         } else {
             val enableBT = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             pendingScanResult = result
-            activity.startActivityForResult(enableBT, REQUEST_BLUETOOTH)
+            activityBinding.activity.startActivityForResult(enableBT, REQUEST_BLUETOOTH)
         }
     }
 }
